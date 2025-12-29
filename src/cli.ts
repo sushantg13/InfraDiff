0e5e9d85fd9c46cb49223a8090b4232d2d0d9bcc
sushantg13 <sushantgupta13@outlook.com>
2025-12-30 00:02:23 +1100

    feat: snapshot command, basics implemented, output: JSON

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-console.log("Infra Diff is alive :)");
+const command = process.argv[2];
+
+if(command === "snapshot"){
+    const snapshot = {
+        meta:{
+            tool: "infradiff",
+            version: "0.1.0",
+            createdAt: new Date().toISOString(),
+            projectRoot: process.cwd(),
+        },
+        env: {},
+        dependencies: {},
+    };
+
+    //printing the snapshot
+    console.log(JSON.stringify(snapshot, null, 2));
+}
+else {
+    console.log("Unknown command: Try 'snapshot' ");
+}
